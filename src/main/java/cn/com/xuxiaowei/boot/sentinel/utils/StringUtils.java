5007636aafa404fc377a4d0cf61e69a62f9c3b30
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-02-26 11:42:16 +0800

    :bug: 空值处理

--- src/main/java/cn/com/xuxiaowei/boot/sentinel/utils/StringUtils.java
+++ src/main/java/cn/com/xuxiaowei/boot/sentinel/utils/StringUtils.java
@@ -1,6 +1,7 @@
 package cn.com.xuxiaowei.boot.sentinel.utils;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -18,6 +19,9 @@ public class StringUtils {
 	 * @return Map
 	 */
 	public static Map<String, String> stringToMap(String input) {
+		if (input == null) {
+			return new HashMap<>();
+		}
 		return Arrays.stream(input.split(","))
 			.map(pair -> pair.split("="))
 			.filter(keyValue -> keyValue.length == 2)
